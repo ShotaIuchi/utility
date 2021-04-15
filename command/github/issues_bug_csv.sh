fb02bf3aa351b820531d456afea487817c1ea980
Shota Iuchi <shotaiuchi.develop@gmail.com>
2021-04-15 23:24:56 +0900

    command:git:issue_bug_csv.sh:1000件

--- command/github/issues_bug_csv.sh
+++ command/github/issues_bug_csv.sh
@@ -1,6 +1,69 @@
 curl \
  -H "Accept: application/vnd.github.v3+json" \
- -G --data "labels=bug" \
+ -G --data "labels=bug" --data "sort=updated" \
  "https://api.github.com/repos/ShotaIuchi/githubtest/issues" \
  | jq -r '["id","title"], (.[] | [.id,.title]) | @csv' \
  > issues.csv
+
+curl \
+ -H "Accept: application/vnd.github.v3+json" \
+ -G --data "labels=bug" --data "sort=updated" --data "page=2" \
+ "https://api.github.com/repos/ShotaIuchi/githubtest/issues" \
+ | jq -r '(.[] | [.id,.title]) | @csv' \
+ >> issues.csv
+
+curl \
+ -H "Accept: application/vnd.github.v3+json" \
+ -G --data "labels=bug" --data "sort=updated" --data "page=3" \
+ "https://api.github.com/repos/ShotaIuchi/githubtest/issues" \
+ | jq -r '(.[] | [.id,.title]) | @csv' \
+ >> issues.csv
+
+curl \
+ -H "Accept: application/vnd.github.v3+json" \
+ -G --data "labels=bug" --data "sort=updated" --data "page=4" \
+ "https://api.github.com/repos/ShotaIuchi/githubtest/issues" \
+ | jq -r '(.[] | [.id,.title]) | @csv' \
+ >> issues.csv
+
+curl \
+ -H "Accept: application/vnd.github.v3+json" \
+ -G --data "labels=bug" --data "sort=updated" --data "page=5" \
+ "https://api.github.com/repos/ShotaIuchi/githubtest/issues" \
+ | jq -r '(.[] | [.id,.title]) | @csv' \
+ >> issues.csv
+
+curl \
+ -H "Accept: application/vnd.github.v3+json" \
+ -G --data "labels=bug" --data "sort=updated" --data "page=6" \
+ "https://api.github.com/repos/ShotaIuchi/githubtest/issues" \
+ | jq -r '(.[] | [.id,.title]) | @csv' \
+ >> issues.csv
+
+curl \
+ -H "Accept: application/vnd.github.v3+json" \
+ -G --data "labels=bug" --data "sort=updated" --data "page=7" \
+ "https://api.github.com/repos/ShotaIuchi/githubtest/issues" \
+ | jq -r '(.[] | [.id,.title]) | @csv' \
+ >> issues.csv
+
+curl \
+ -H "Accept: application/vnd.github.v3+json" \
+ -G --data "labels=bug" --data "sort=updated" --data "page=8" \
+ "https://api.github.com/repos/ShotaIuchi/githubtest/issues" \
+ | jq -r '(.[] | [.id,.title]) | @csv' \
+ >> issues.csv
+
+curl \
+ -H "Accept: application/vnd.github.v3+json" \
+ -G --data "labels=bug" --data "sort=updated" --data "page=9" \
+ "https://api.github.com/repos/ShotaIuchi/githubtest/issues" \
+ | jq -r '(.[] | [.id,.title]) | @csv' \
+ >> issues.csv
+
+curl \
+ -H "Accept: application/vnd.github.v3+json" \
+ -G --data "labels=bug" --data "sort=updated" --data "page=10" \
+ "https://api.github.com/repos/ShotaIuchi/githubtest/issues" \
+ | jq -r '(.[] | [.id,.title]) | @csv' \
+ >> issues.csv
